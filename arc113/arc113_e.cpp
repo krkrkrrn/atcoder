48dd04064ee4aa8a2ac3c28e409e4cced3a4c8cc
krkrkrrn <kloud727@gmail.com>
2021-03-07 17:16:46 +0900

    on the way.

--- arc113/arc113_e.cpp
+++ arc113/arc113_e.cpp
@@ -35,11 +35,68 @@ unsigned long bit_reverse(unsigned long n, int l) {
     return res;
 }
 
-unsigned long part_reverse() {
+unsigned long part_reverse(unsigned long n, int begin, int end) {
+    // bitset<32> print_bit(n);
+    // DEBUG_VAL("n", print_bit);
+
+    // create bit mask
+    unsigned long bit_mask = ((1 << (end + 1)) - 1) ^ ((1 << begin) - 1);
+    unsigned long x = n & bit_mask;
+
+    // print_bit = x;
+    // DEBUG_VAL("x", print_bit);
+
+    x >>= begin;
+
+    // print_bit = x;
+    // DEBUG_VAL("x >>= ", print_bit);
+
+    unsigned long res = bit_reverse(x, (end - begin + 1));
+
+    // print_bit = res;
+    // DEBUG_VAL("bit_reverse", print_bit);
+
+    res <<= begin;
+
+    // print_bit = res;
+    // DEBUG_VAL("res <<= ", print_bit);
+
+    // print_bit = (n & ~bit_mask);
+    // DEBUG_VAL("n & ~", print_bit);
+
+    res = res | (n & ~bit_mask);
     
+    // print_bit = res;
+    // DEBUG_VAL("out", print_bit);
+    return res;
 } 
 
-void rvom_and_rsrev(string s) {
+bool rvom_and_rsrev(unsigned long& bit, int bit_length) {
+    unsigned long min = bit;
+    int begin, end;
+    for(int i = 0; i < bit_length; i++) {
+        for(int j = i + 2; j < bit_length; j++) {
+            if(((bit >> i) & 1) == ((bit >> j) & 1)) {
+                unsigned long n = part_reverse(bit, i, j);
+                if(min > n) {
+                    bitset<16> print_bit(n);
+                    DEBUG_VAL("min > n", print_bit);
+                    min = n;
+                    begin = i;
+                    end = j;
+                }
+            } 
+        }
+    }
+    if(bit > min) {
+        bit = min;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void read_string(string s) {
     unsigned long ab = 0;
     int ab_length = s.size();
     for(int i = 0; i < s.size(); i++) {
@@ -48,12 +105,20 @@ void rvom_and_rsrev(string s) {
         }
     }
     
-
+    int count = 1000;
+    while(rvom_and_rsrev(ab, ab_length)) {
+        if(count < 0) {
+            DEBUG_DONE("over 1000 loop.");
+            break;
+        }
+        count--;
+    }
+    
     bitset<32> print_bit(ab);
     DEBUG_VAL("0b", print_bit);
-    long debug_re = bit_reverse(ab, ab_length);
-    bitset<32> print_bit2(debug_re);
-    DEBUG_VAL("re : 0b", print_bit2);
+    // long debug_re = bit_reverse(ab, ab_length);
+    // bitset<32> print_bit2(debug_re);
+    // DEBUG_VAL("re : 0b", print_bit2);
 }
 
 
@@ -69,7 +134,7 @@ int main() {
     }
 
     for(const auto s : S) {
-        rvom_and_rsrev(s);
+        read_string(s);
     }
 
     // DEBUG_DONE("print S");
